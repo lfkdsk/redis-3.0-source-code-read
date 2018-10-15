1f15b44024fa8109fbd5d41ae59cc903caf86273
liufengkai <liufengkai@bytedance.com>
2018-10-15 16:01:07 +0800

    rdb

--- src/rdb.c
+++ src/rdb.c
@@ -40,6 +40,8 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 
+// write or read 方法
+
 static int rdbWriteRaw(rio *rdb, void *p, size_t len) {
     if (rdb && rioWrite(rdb,p,len) == 0)
         return -1;
